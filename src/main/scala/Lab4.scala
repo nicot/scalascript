680eb442380f20baa6bd8cbd2b07b276540c2573
Nico Tonozzi <dominic.tonozzi@gmail.com>
2014-03-13 12:20:53 -0600

    Passed compression tests

--- src/main/scala/Lab4.scala
+++ src/main/scala/Lab4.scala
@@ -15,12 +15,15 @@ object Lab4 extends jsy.util.JsyApplication {
   /* Lists */
   
   def compressRec[A](l: List[A]): List[A] = l match {
-    case Nil | _ :: Nil => throw new UnsupportedOperationException
-    case h1 :: (t1 @ (h2 :: _)) => throw new UnsupportedOperationException
+    case Nil | _ :: Nil => l
+    case h1 :: (t1 @ (h2 :: _)) => if (h1 == h2) compressRec(t1) else h1::compressRec(t1)
   }
   
   def compressFold[A](l: List[A]): List[A] = l.foldRight(Nil: List[A]){
-    (h, acc) => throw new UnsupportedOperationException
+    (h, acc) => (h, acc) match { 
+      case (h, Nil) => List(h)
+      case (h, acc) => println(h,acc); if (acc(0) ==h) acc else h::acc
+    }
   }
   
   def mapFirst[A](f: A => Option[A])(l: List[A]): List[A] = l match {
